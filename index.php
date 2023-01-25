b9df27b1d0ba41527c8db0e6aec78fe30a300a2f
LorenzoOgnibene <lorenzo.ognibene@gmail.com>
2023-01-25 17:16:09 +0100

    add table for each hotel

--- index.php
+++ index.php
@@ -8,8 +8,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
     </head>
-    <body class="container">
-        <div class="row">
+    <body>
                 <?php
                     $hotels = [
                         [
@@ -50,18 +49,46 @@
 
                     ];
                     
-                    foreach($hotels as $key => $hotel){
-                        echo "<div class='col-5 border border-secondary g-3 me-autod '>";
-                        echo "<p>Nome: {$hotel['name']} </p>";
-                        echo "<p>Descrizione: {$hotel['description']} </p>";
-                        echo "<p>Parcheggio: {$hotel['parking']} </p>";
-                        echo "<p>Voto: {$hotel['vote']} </p>";
-                        echo "<p>Distanza dal centro: {$hotel['distance_to_center']}mt </p>";
-                        echo "</div>";
-                    }
+                    
+                    //foreach($hotels as $key => $hotel){
+                    //    echo "<div class='col-5 border border-secondary g-3 me-autod '>";
+                    //    echo "<p>Nome: {$hotel['name']} </p>";
+                     //   echo "<p>Descrizione: {$hotel['description']} </p>";
+                    //    echo "<p>Parcheggio: {$hotel['parking']} </p>";
+                    //    echo "<p>Voto: {$hotel['vote']} </p>";
+                    //    echo "<p>Distanza dal centro: {$hotel['distance_to_center']}mt </p>";
+                    //    echo "</div>";
+                   // }
                     ?>
 
+        <div class="container pt-5">
+            <table class="table table-primary">
+                <?php
+                    echo " <thead>
+                                <tr>
+                                    <th scope='col'>Nome</th>
+                                    <th scope='col'>Descrizione</th>
+                                    <th scope='col'>Parcheggio</th>
+                                    <th scope='col'>Voto</th>
+                                    <th scope='col'>Distanza dal centro</th>
+                                </tr>
+                        </thead>";
+                    echo "<tbody>";
+                    foreach ($hotels as $hotel) {
+                        echo "<tr>";
+                        echo "<td>" . $hotel['name'] . "</td>";
+                        echo "<td>" . $hotel['description'] . "</td>";
+                        echo "<td>" . $hotel['parking'] . "</td>";
+                        echo "<td>" . $hotel['vote'] . "</td>";
+                        echo "<td>" . $hotel['distance_to_center'] . "</td>";
+                        echo "</tr>";
+                    }
+                    echo "</tbody>";
+                ?>
+            </table>
         </div>
+
+        
         
     </body>
 </html>
